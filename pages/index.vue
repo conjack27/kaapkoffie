056c84c92db6e59b8736bf6eb3af38173b5d4954
conjack27 <38353145+conjack27@users.noreply.github.com>
2024-01-02 21:31:31 +0100

    home page stylin

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,42 @@
       <h1 class="home__heading">The cleanest coffee on the planet</h1>
     </div>
     <div class="horizontal-line"></div>
+    <div class="home__image-section">
+      <img
+        class="home__image"
+        src="https://taupe-mochi-8cdda8.netlify.app/images/beans-boat.jpg"
+        alt="Coffee beans"
+      />
+      <div class="home__coffee-cups">
+        <div class="home__coffee-cup">
+          <img
+            class="home__coffee-cup-image"
+            src="https://taupe-mochi-8cdda8.netlify.app/images/koffiekopje.png"
+            alt="Coffee beans"
+          />
+          <p class="home__coffee-cup-text home__coffee-cup-text--1">Try now</p>
+        </div>
+        <div class="home__coffee-cup">
+          <img
+            class="home__coffee-cup-image"
+            src="https://taupe-mochi-8cdda8.netlify.app/images/koffiekopje.png"
+            alt="Coffee beans"
+          />
+          <p class="home__coffee-cup-text home__coffee-cup-text--2">
+            Follow the bean
+          </p>
+        </div>
+        <div class="home__coffee-cup">
+          <img
+            class="home__coffee-cup-image"
+            src="https://taupe-mochi-8cdda8.netlify.app/images/koffiekopje.png"
+            alt="Coffee beans"
+          />
+          <p class="home__coffee-cup-text home__coffee-cup-text--3">
+            Tasting profile
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
