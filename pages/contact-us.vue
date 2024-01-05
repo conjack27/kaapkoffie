7b61b3383cd78b16270f188fa0e833376b4f0bcb
conjack27 <38353145+conjack27@users.noreply.github.com>
2024-01-05 15:20:56 +0100

    contact form image changes

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="contact">
     <div class="contact__form">
-      <h1>Contact us</h1>
+      <h1 class="contact__form-heading">Contact us</h1>
 
       <form class="contact__form-fields" action="">
         <div class="contact__form-section">
@@ -34,12 +34,16 @@
         <input class="contact__form-button" type="submit" value="Send" />
       </form>
       <img
-        class="contact__image"
+        class="contact__image--mobile"
         src="https://taupe-mochi-8cdda8.netlify.app/images/cup-of-coffee.png"
         alt="person holding a cup of coffee"
       />
     </div>
-
+    <img
+      class="contact__image--desktop"
+      src="https://taupe-mochi-8cdda8.netlify.app/images/cup-of-coffee.png"
+      alt="person holding a cup of coffee"
+    />
     <div class="contact__footer">
       <div class="contact__address"></div>
       <div class="contact__details"></div>
