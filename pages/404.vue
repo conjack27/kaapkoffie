47cb64d5109f73551b4ce6ae69ec79131059000c
conjack27 <38353145+conjack27@users.noreply.github.com>
2024-01-05 16:32:04 +0100

    form submission logic

---
+++ pages/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Oops! Something went wrong.</h1>
+    <p>Please go back to the <nuxt-link to="/">home page</nuxt-link>.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ErrorPage',
+  };
+</script>
---
+++ pages/thanks.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Thanks for your submission!</h1>
+    <p>We appreciate your feedback.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Thanks',
+  };
+</script>
+
+<style scoped>
+  h1 {
+    color: green;
+  }
+
+  p {
+    font-size: 18px;
+  }
+</style>
