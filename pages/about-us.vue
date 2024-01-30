607be902e8842130eb7916bdf4526a51caad3dc5
conjack27 <38353145+conjack27@users.noreply.github.com>
2024-01-30 18:27:30 +0100

    added images

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,34 +1,29 @@
 <template>
-  <div class="hero__section">
-    <div class="hero hero--full-screen hero--us">
-      <div class="hero__heading-container">
-        <h1 class="hero__heading">About Us</h1>
-        <blockquote class="hero__quote">
-          "We believe in sustainable maritime transportation."
-        </blockquote>
-        <blockquote class="hero__quote">
-          "Together, we can make a difference."
-        </blockquote>
+  <div class="content content--about-us">
+    <div class="hero__section">
+      <div class="hero hero--full-screen hero--us">
+        <div class="hero__heading-container">
+          <h1 class="hero__heading">About Us</h1>
+          <blockquote class="hero__quote">
+            "We believe in sustainable maritime transportation."
+          </blockquote>
+          <blockquote class="hero__quote">
+            "Together, we can make a difference."
+          </blockquote>
+        </div>
       </div>
-    </div>
-    <div class="text">
-      <p class="text__paragraph">
-        KaapKargo was founded in 2022 to make the maritime industry more
-        sustainable.
-      </p>
-      <p class="text__paragraph">
-        We have the ambition to establish a fleet of sailing cargo vessels. Each
-        ship makes two crossings per year, creating a reliable and logistical
-        scheduled service.
-      </p>
-    </div>
-    <div class="text-asset">
-      <div class="text-asset__text-container">
-        <h2 class="text-asset__heading">{{ title }}</h2>
-        <p class="text-asset__text">{{ text }}</p>
-      </div>
-      <div class="image-container">
-        <img class="text-asset__image" :src="imageSrc" :alt="imageAlt" />
+      <div class="text-asset">
+        <div class="text-asset__text-container">
+          <h2 class="text-asset__heading">{{ title }}</h2>
+          <p class="text-asset__text">{{ text }}</p>
+          <p class="text-asset__text">{{ text2 }}</p>
+          <NuxtLink to="/contact-us" class="btn btn__primary"
+            >Join our team!</NuxtLink
+          >
+        </div>
+        <div class="image-container">
+          <img class="text-asset__image" :src="imageSrc" :alt="imageAlt" />
+        </div>
       </div>
     </div>
   </div>
@@ -38,56 +33,17 @@
   export default {
     data() {
       return {
-        title: 'Sample Title',
-        text: 'Sample Text',
+        title: 'What now?',
+        text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+        text2:
+          'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
         imageSrc:
           'https://taupe-mochi-8cdda8.netlify.app/images/father-and-son.jpg',
-        imageAlt: 'Image Alt Text',
+        imageAlt: 'Two sailors on a boat holding a bag of coffee beans',
       };
     },
     mounted() {},
   };
 </script>
 
-<style lang="scss">
-  .text-asset {
-    background-color: #f9f8f9;
-    display: flex;
-    flex-direction: column;
-
-    @media (min-width: 600px) {
-      flex-direction: row;
-    }
-  }
-
-  .text-asset__text-container {
-    order: 2;
-    padding: 15px;
-
-    @media (min-width: 600px) {
-      order: 1;
-      flex: 1;
-    }
-  }
-
-  .image-container {
-    padding-top: 15px;
-    order: 1;
-    width: 100%;
-    height: auto;
-
-    @media (min-width: 600px) {
-      order: 2;
-      flex: 1;
-    }
-  }
-
-  .text-asset__image {
-    width: 100%;
-    height: auto;
-  }
-
-  .text-asset__heading {
-    margin: 0;
-  }
-</style>
+<style lang="scss"></style>
