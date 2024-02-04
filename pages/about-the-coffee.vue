a017a372715fa889b96f2b5e4024ddc0ec9c52e5
conjack27 <38353145+conjack27@users.noreply.github.com>
2024-02-04 17:31:40 +0800

    faq styling

--- pages/about-the-coffee.vue
+++ pages/about-the-coffee.vue
@@ -1,34 +1,43 @@
 <template>
-  <div class="hero__section">
-    <div
-      class="hero hero--coffee hero--full-screen"
-      style="background-image: url('/images/my-hand.jpg')"
-    >
-      <div class="hero__notes">
-        <h2 class="hero__sub-heading">About the Coffee</h2>
-        <h4 class="hero__notes-heading">Roast profile</h4>
-        <p class="hero__notes-text">Light toast, Medium roast & Dark roast</p>
-        <h4 class="hero__notes-heading">Recommended Use</h4>
-        <p class="hero__notes-text">Drip, Pour-Over</p>
-        <h4 class="hero__notes-heading">Cupping Notes</h4>
-        <p class="hero__notes-text">
-          Well-balanced acidity and body. Smooth taste profile with a somewhat
-          nutty finish
+  <div class="coffee">
+    <section class="coffee__top">
+      <div class="coffee__content">
+        <h1 class="coffee__heading">About the coffee.</h1>
+        <p class="coffee__paragraph-title">Roast Profile</p>
+        <p class="coffee__paragraph">Light Roast, Medium Roast & Dark•Roast</p>
+        <p class="coffee__paragraph-title">Recommended Use</p>
+        <p class="coffee__paragraph">Drip, Pour-Over</p>
+        <p class="coffee__paragraph-title">Cupping Notes</p>
+        <p class="coffee__paragraph">
+          Well balánced acidity and body. Smooth taste profile with a somewhat
+          nutty finish.
         </p>
       </div>
-      <h1 class="hero__heading">Where are they grown?</h1>
-    </div>
-    <div class="text">
-      <p class="text__paragraph">Sample text</p>
-      <p class="text__paragraph">Sampe text</p>
-    </div>
-    <div class="text-asset">
-      <div class="text-asset__text-container">
-        <h2 class="text-asset__heading">{{ title }}</h2>
-        <p class="text-asset__text">{{ text }}</p>
-      </div>
-      <div class="text-asset__image-container">
-        <img class="text-asset__image" :src="imageSrc" :alt="imageAlt" />
+    </section>
+    <div class="coffee__section-bottom">
+      <img
+        src="/images/jungle.jpg"
+        alt="Lush, verdant jungle"
+        class="coffee__image"
+      />
+      <div class="coffee__text">
+        <h2 class="coffee__heading coffee__heading--bottom">
+          And where are they grown?
+        </h2>
+        <p class="coffee__paragraph coffee__paragraph--bottom">
+          Our Colombia Organic Sierra Nevada is produced by just 78 family
+          growers, located within the isolated Sierra Nevada de Santa Marta
+          coastal mountain range in Northern Colombia's Magdalena Department.
+          Their co-op, The CooAgroNevada, also known as The Cooperative of
+          Sierra Nevada and Santa Marta Coffee Growers and Farmers, unites these
+          coffee growing families from 19 different villages with the combined
+          goals to improve social welfare, economic growth, quality of coffee
+          and eco-tourism while preserving the region's picturesque peaks,
+          rivers and valleys. The Colombia Organic Sierra Nevada is also
+          designated as EP or "European Prep" — in the cup it achieves a very
+          nice balance between acidity and body while delivering an overall
+          smooth and
+        </p>
       </div>
     </div>
   </div>
@@ -38,15 +47,153 @@
   export default {
     data() {
       return {
-        title: 'Sample Title',
-        text: 'Sample Text',
-        imageSrc:
-          'https://images.unsplash.com/photo-1570828066702-7706220a65f6?q=80&w=2831&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        imageAlt: 'Image Alt Text',
+        imageSrc: 'https://taupe-mochi-8cdda8.netlify.app/',
+        isDesktop: false,
+        imageAlt: 'Bag of Kaap Koffie coffee beans',
       };
     },
-    mounted() {},
+    mounted() {
+      this.isDesktop = window.innerWidth > 600;
+    },
   };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+  @import '../assets/css/modules/variables.scss';
+
+  .coffee {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    @media (min-width: 600px) {
+      background-position: bottom;
+    }
+
+    &__top {
+      padding: 20px;
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center center;
+      background-image: url('/images/about-the-coffee.jpg');
+
+      @media (min-width: 600px) {
+        height: 90vh;
+      }
+    }
+
+    &__content {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+      margin-top: 20px;
+      padding: 20px;
+
+      @media (min-width: 600px) {
+        margin-top: 0;
+        padding-left: 20px;
+        height: 100%;
+      }
+    }
+
+    &__heading {
+      font: $pageHeading;
+      color: $black-font;
+      margin-bottom: 200px;
+
+      @media (min-width: 600px) {
+        margin-bottom: 0;
+      }
+    }
+
+    &__paragraph-title {
+      color: $black-font;
+      font: $paragraph;
+      font-weight: bold;
+      margin-bottom: 0;
+      font-size: 24px;
+
+      @media (min-width: 600px) {
+        width: 30%;
+      }
+    }
+
+    &__paragraph {
+      margin-bottom: 30px;
+      margin-top: 0;
+      font: $paragraph;
+      color: $black-font;
+
+      @media (min-width: 600px) {
+        font-size: 24px;
+        width: 30%;
+      }
+    }
+
+    &__section-bottom {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 20px;
+      background-color: #000;
+
+      @media (min-width: 600px) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: flex-start;
+      }
+    }
+
+    &__heading--bottom {
+      font: $pageHeading;
+      color: $white-font;
+      margin-bottom: 20px;
+      margin-top: 0;
+
+      @media (min-width: 600px) {
+        margin-left: -200px;
+      }
+    }
+
+    &__image {
+      width: 100%;
+      height: 300px;
+      order: 2;
+
+      @media (min-width: 600px) {
+        display: block;
+        flex: 0 1 50%;
+        height: 80%;
+        width: 80%;
+        order: 1;
+      }
+    }
+
+    &__text {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+      order: 1;
+      padding: 20px;
+      width: 100%;
+
+      @media (min-width: 600px) {
+        flex: 0 1 50%;
+        order: 2;
+      }
+    }
+
+    &__paragraph--bottom {
+      font: $paragraph;
+      margin-top: 0;
+      color: $white-font;
+
+      @media (min-width: 600px) {
+        width: 100%;
+      }
+    }
+  }
+</style>
--- pages/faq.vue
+++ pages/faq.vue
@@ -1,19 +1,118 @@
 <template>
-  <div class="content">
-    <div class="hero hero--team hero--standard">
-      <h1 class="hero__heading">Want to be a part of our team?</h1>
-    </div>
-    <div class="content__page content__btns">
-      <nuxtLink to="/contact-us" class="btn btn__primary"
-        >Work on land</nuxtLink
-      >
-      <a class="btn btn__primary" href="https://www.kaapkargo.com/crew">
-        Work at sea
-      </a>
+  <div class="faq">
+    <h1 class="faq__heading">Frequently Asked Questions</h1>
+    <div class="faq__coffee-beans faq__coffee-beans--top"></div>
+    <div class="faq__content">
+      <div class="faq__block">
+        <strong class="faq__question">What is specialty coffee?</strong>
+        <p class="faq__answer">
+          Specialty coffee refers to high-quality coffee that is carefully
+          produced and processed. Specialty coffee typically involves beans
+          grown in specific regions with distinct climates and altitudes,
+          contributing to the coffee's taste profile. The grading of specialty
+          coffee often considers factors like bean size, lack of defects, and
+          flavor complexity. Dedicated roasting techniques further contribute to
+          the beans' qualities, resulting in a premium and distinct coffee
+          experience. In essence, specialty coffee represents a commitment to
+          excellence throughout the entire coffee production chain.
+        </p>
+      </div>
+      <div class="faq__block">
+        <strong class="faq__question">Is Kaapkoffie organic?</strong>
+        <p class="faq__answer">Yes! All our coffee is organic.</p>
+        <p class="faq__answer">
+          For coffee to be considered organic, not only our coffee beans must be
+          organically certified, but also our entire supply chain, including
+          farmers, transportation, roaster, and storage. Kaapkoffie adheres to
+          these regulations and has been bio-certified by Skal Biocontrole.
+        </p>
+      </div>
+      <div class="faq__block">
+        <strong class="faq__question"
+          >What type of packaging do you have?</strong
+        >
+        <p class="faq__answer">
+          At Kaapkoffie, we aim for sustainability throughout our entire supply
+          chain, including our packaging. That's why we use biodegradable and
+          reusable packaging. Biodegradable means our packaging can be broken
+          down by nature's microorganisms.
+        </p>
+      </div>
+      <div class="faq__block">
+        <strong class="faq__question">Where do you roast your coffee?</strong>
+        <p class="faq__answer">
+          We roast our coffee at ID Organics in Zaandam, Netherlands The round
+          trip to Colombia takes about five months.Roasting locally ensures the
+          freshness of our beans. IDorganics is an organic food manufacturer.
+          Approximately 40% of their employees have a disability or are
+          otherwise distanced from the labor market. Each employee has a
+          personal buddy and a protected workplace, ensuring a safe and
+          enjoyable work environment.
+        </p>
+      </div>
+      <div class="faq__block">
+        <strong class="faq__question">Why a female farmer corporation?</strong>
+        <p class="faq__answer">
+          At Kaapkoffie, our collaboration with a female-led corporation goes
+          beyond business. Many of the women behind this provider for their
+          tamily Additionaly some face societal expectations urging women to
+          stay at home. Wete dedicated to supporting these women, helping them
+          build a secure future for themselves and their families.
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<style lang="scss">
+  @import '../assets/css/modules/variables.scss';
+  .faq {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px 10%;
+    background-color: $white-font;
+    position: relative;
 
-<style></style>
+    @media (min-width: 600px) {
+      padding: 20px 20%;
+    }
+
+    &__heading {
+      font: $pageHeading;
+      margin-bottom: 20px;
+      z-index: 1000;
+
+      @media (min-width: 600px) {
+      }
+    }
+
+    &__content {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+    }
+
+    &__block {
+      margin-bottom: 10px;
+    }
+
+    &__coffee-beans {
+      width: 600px;
+      height: 500px;
+      top: 0;
+      background-image: url('/images/koffie.png');
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center center;
+      margin-bottom: 20px;
+      position: absolute;
+      z-index: 999;
+
+      &--top {
+        height: 200px;
+        margin-bottom: 40px;
+      }
+    }
+  }
+</style>
