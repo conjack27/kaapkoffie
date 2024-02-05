95fe4663037df172a15123a00321ec2c3ce9289f
conjack27 <38353145+conjack27@users.noreply.github.com>
2024-02-05 10:37:52 +0800

    added blog

--- pages/index.vue
+++ pages/index.vue
@@ -51,6 +51,52 @@
         allowfullscreen
       ></iframe>
     </div>
+
+    <h2 class="home__blog-heading">Voyage diaries</h2>
+    <div class="home__blog">
+      <div class="home__blog-item">
+        <img
+          class="home__blog__img"
+          src="/images/voyage-diary-1.png"
+          alt="People on boat"
+        />
+        <h3 class="home__blog-title">Part 1</h3>
+        <p class="home__blog-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis.
+        </p>
+      </div>
+      <div class="home__blog-item">
+        <img
+          class="home__blog__img"
+          src="/images/voyage-diary-1.png"
+          alt="People on boat"
+        />
+        <h3 class="home__blog-title">Part 2</h3>
+        <p class="home__blog-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis.
+        </p>
+      </div>
+      <div class="home__blog-item">
+        <img
+          class="home__blog__img"
+          src="/images/voyage-diary-1.png"
+          alt="People on boat"
+        />
+        <h3 class="home__blog-title">Part 3</h3>
+        <p class="home__blog-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
