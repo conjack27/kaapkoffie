dbdfff13738a47df230ef50f579e6ee449ba18f9
conjack27 <38353145+conjack27@users.noreply.github.com>
2024-02-05 10:07:05 +0800

    finished journey

--- pages/sailing-journey.vue
+++ pages/sailing-journey.vue
@@ -1,25 +1,46 @@
 <template>
   <div class="journey">
     <div class="journey__top">
-      <h1>About the journey</h1>
+      <h1 class="journey__heading">About the journey</h1>
     </div>
     <div class="journey__content">
       <div class="journey__paragraphs">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <p>
-          Nulla facilisi. Sed euismod mauris non tellus tincidunt, eu aliquet
-          nisl ultrices.
+        <p class="journey__paragraph">
+          Our coffee beans are sailed by Kaapkargo, which was founded in 2022.
+          To combat pollution within the maritime industry, Kaapkargo offers
+          sailing cargo vessels as a sustainable alternative to traditional
+          container ships. Kaapkargo strives to support global climate goals by
+          utilizing wind power and human effort instead of depending on fossil
+          fuels.
         </p>
-        <p>
-          Proin auctor, nunc id aliquam tincidunt, nisl nunc ultrices nunc, id
-          aliquam nisl nunc ut nunc.
+        <p class="journey__paragraph">
+          Founded in 2022, KaapKargo is proud to be more than just a cargo
+          company; we are pioneers in sustainable shipping. Breaking free from
+          the conventional, we've set sail with the Ide Min, crossing the
+          Atlantic Ocean with 60 tons of organic coffee beans from Colombia to
+          the heart of Amsterdam. This journey can be made twice a year,
+          traveling there and back for approximately five months (depending on
+          the wind) But it's not just a journey across the seas; it's a
+          commitment to a greener tomorrow. Our second ship, currently under
+          construction, will boast a capacity of 120 tons, doubling our curousin
+          fostering sustain atia looks like, follow our
+          <NuxtLink to="/">Voyage Diaries</NuxtLink>
         </p>
-        <p>
-          Donec auctor, nunc id aliquam tincidunt, nisl nunc ultrices nunc, id
-          aliquam nisl nunc ut nunc.
+        <p class="journey__paragraph">
+          The combination of our shipbuilding skills and entrepreneurial
+          experience drives our ambition to establish a fleet of sailing cargo
+          vessels.
+        </p>
+        <p class="journey__paragraph">
+          Questions or inquiries? Feel free to reach out!
         </p>
       </div>
-      <a class="journey__cta" href="https://kaapkargo.com">Learn More</a>
+      <a
+        class="journey__cta btn btn__secondary btn--large"
+        target="_blank"
+        href="https://kaapkargo.com"
+        >KaapKargo</a
+      >
     </div>
     <div class="journey__video">
       <video controls>
@@ -30,27 +51,39 @@
 </template>
 
 <style lang="scss">
+  @import '../assets/css/modules/variables.scss';
   .journey {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100vh;
-    width: 100vw;
+    margin-bottom: 20px;
 
     &__top {
-      background-image: url('path/to/background-image.jpg');
+      background-image: url('/images/boat.jpg');
       background-size: cover;
       background-position: center;
       display: flex;
-      align-items: center;
+      padding: 20px;
+      align-items: flex-start;
       justify-content: center;
-      height: 100%;
       width: 100%;
 
-      h1 {
-        color: white;
-        font-size: 2rem;
+      @media (min-width: 600px) {
+        height: 92vh;
+        padding-top: 200px;
+        align-items: flex-start;
+        justify-content: flex-start;
+      }
+    }
+
+    &__heading {
+      color: $white-font;
+      font: $pageHeading;
+
+      @media (min-width: 600px) {
+        color: $black-font;
+        font: 72px;
       }
     }
 
@@ -59,23 +92,11 @@
       flex-direction: column;
       align-items: center;
       justify-content: center;
-      margin-top: 2rem;
+      padding: 20px;
+    }
 
-      &__paragraphs {
-        margin-bottom: 2rem;
-
-        p {
-          margin-bottom: 1rem;
-        }
-      }
-
-      &__cta {
-        padding: 1rem 2rem;
-        background-color: blue;
-        color: white;
-        text-decoration: none;
-        font-weight: bold;
-      }
+    &__paragraph {
+      font-size: 18px;
     }
 
     &__video {
